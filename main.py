fb6b773a7f2d3782ecf55af4acd5bc60868df904
Bilal-Ali99 <bilal.ali1999@gmail.com>
2025-05-26 21:36:03 +0500

    Using New Template for Output

--- main.py
+++ main.py
@@ -15,5 +15,51 @@ llm = ChatGoogleGenerativeAI(
     temperature = "0.7"
 )
 
-response = llm.invoke("explain the quantum computing in 5 lines")
-print(response.text)
+# response = llm.invoke("explain the quantum computing in 5 lines")
+# print(response.content)
+
+# template = """ you are an assistant. Answer the question clearly and precisely 
+# Question = {question}
+# Answer:
+# """
+
+# prompt = PromptTemplate(
+#     input_variables=["question"],
+#     template= template
+
+# )
+
+# formatted_prompt = prompt.format(question = "what is quantum physics")
+# response = llm.invoke(formatted_prompt)
+# print(response.content)
+
+advance_template = """
+    
+    you are a {Role} expert.
+    Context: {Context}
+    Task: {Task}
+     
+    Please provide a {Style} response
+
+    Input:{Input} 
+    
+    """
+advance_prompt = PromptTemplate(
+    input_variables=["Role","Context","Task","Style","Input"] ,
+    template=advance_template
+)
+
+advance_format = advance_prompt.format(
+    Role = "Python Programming",
+    Context = "Teaching a beginner",
+    Task = "Explain the concept",
+    Style = "Friendly and Understanding",
+    Input = "What are functions?"
+)
+
+
+
+
+response = llm.invoke(advance_format)
+print(response.content)
+
